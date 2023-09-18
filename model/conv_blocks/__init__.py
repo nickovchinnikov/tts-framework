ec486a8a0ddc3c5801054ce4980fa52fecf2eb4a
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-09-18 16:30:59 +0200

    Fix __init__

--- model/conv_blocks/__init__.py
+++ model/conv_blocks/__init__.py
@@ -1,7 +1,7 @@
-from activation import GLUActivation
-from add_coords import AddCoords
-from bsconv import BSConv1d
-from conv_transposed import ConvTransposed
-from conv1d_glu import Conv1dGLU
-from conv1d import PointwiseConv1d, DepthWiseConv1d
-from coord_conv1d import CoordConv1d
+from .activation import GLUActivation
+from .add_coords import AddCoords
+from .bsconv import BSConv1d
+from .conv_transposed import ConvTransposed
+from .conv1d_glu import Conv1dGLU
+from .conv1d import PointwiseConv1d, DepthWiseConv1d
+from .coord_conv1d import CoordConv1d
