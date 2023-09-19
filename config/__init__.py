ac5cdfddbb9a3c843c3014e4c6f1dfda5b13a129
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-09-19 18:30:01 +0200

    Fix config

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,1 @@
-from config import *
+from .configs import *
--- config/configs.py
+++ config/configs.py
@@ -24,13 +24,15 @@ class PreprocessingConfig:
     sampling_rate: int = 22050
     use_audio_normalization: bool = True
     workers: int = 11
-    stft: STFTConfig = STFTConfig(
-        filter_length=1024,
-        hop_length=256,
-        win_length=1024,
-        n_mel_channels=100,
-        mel_fmin=20,
-        mel_fmax=11025,
+    stft: STFTConfig = field(
+        default_factory=lambda: STFTConfig(
+            filter_length=1024,
+            hop_length=256,
+            win_length=1024,
+            n_mel_channels=100,
+            mel_fmin=20,
+            mel_fmax=11025,
+        )
     )
     forced_alignment_batch_size: int = 200000
     skip_on_error: bool = True
@@ -61,8 +63,10 @@ class AcousticFinetuningConfig:
     freeze_bert_until = 0
     mcd_gen_max_samples = 400
     only_train_speaker_until = 5000
-    optimizer_config: AcousticTrainingOptimizerConfig = AcousticTrainingOptimizerConfig(
-        learning_rate=0.0002, weight_decay=0.1, lr_decay=0.99999
+    optimizer_config: AcousticTrainingOptimizerConfig = field(
+        default_factory=lambda: AcousticTrainingOptimizerConfig(
+            learning_rate=0.0002, weight_decay=0.1, lr_decay=0.99999
+        )
     )
 
 
@@ -78,8 +82,10 @@ class AcousticPretrainingConfig:
     freeze_bert_until = 4000
     mcd_gen_max_samples = 400
     only_train_speaker_until = 0
-    optimizer_config: AcousticTrainingOptimizerConfig = AcousticTrainingOptimizerConfig(
-        learning_rate=0.0002, weight_decay=0.0, lr_decay=1.0
+    optimizer_config: AcousticTrainingOptimizerConfig = field(
+        default_factory=lambda: AcousticTrainingOptimizerConfig(
+            learning_rate=0.0002, weight_decay=0.0, lr_decay=1.0
+        )
     )
 
 
@@ -120,38 +126,46 @@ class VarianceAdaptorConfig:
 class AcousticENModelConfig:
     speaker_embed_dim: int = 384
     lang_embed_dim: int = 1
-    encoder: ConformerConfig = ConformerConfig(
-        n_layers=4,
-        n_heads=6,
-        n_hidden=384,
-        p_dropout=0.1,
-        kernel_size_conv_mod=7,
-        kernel_size_depthwise=7,
-        with_ff=False,
+    encoder: ConformerConfig = field(
+        default_factory=lambda: ConformerConfig(
+            n_layers=4,
+            n_heads=6,
+            n_hidden=384,
+            p_dropout=0.1,
+            kernel_size_conv_mod=7,
+            kernel_size_depthwise=7,
+            with_ff=False,
+        )
     )
-    decoder: ConformerConfig = ConformerConfig(
-        n_layers=6,
-        n_heads=6,
-        n_hidden=384,
-        p_dropout=0.1,
-        kernel_size_conv_mod=11,
-        kernel_size_depthwise=11,
-        with_ff=False,
+    decoder: ConformerConfig = field(
+        default_factory=lambda: ConformerConfig(
+            n_layers=6,
+            n_heads=6,
+            n_hidden=384,
+            p_dropout=0.1,
+            kernel_size_conv_mod=11,
+            kernel_size_depthwise=11,
+            with_ff=False,
+        )
     )
-    reference_encoder: ReferenceEncoderConfig = ReferenceEncoderConfig(
-        bottleneck_size_p=4,
-        bottleneck_size_u=256,
-        ref_enc_filters=[32, 32, 64, 64, 128, 128],
-        ref_enc_size=3,
-        ref_enc_strides=[1, 2, 1, 2, 1],
-        ref_enc_pad=[1, 1],
-        ref_enc_gru_size=32,
-        ref_attention_dropout=0.2,
-        token_num=32,
-        predictor_kernel_size=5,
+    reference_encoder: ReferenceEncoderConfig = field(
+        default_factory=lambda: ReferenceEncoderConfig(
+            bottleneck_size_p=4,
+            bottleneck_size_u=256,
+            ref_enc_filters=[32, 32, 64, 64, 128, 128],
+            ref_enc_size=3,
+            ref_enc_strides=[1, 2, 1, 2, 1],
+            ref_enc_pad=[1, 1],
+            ref_enc_gru_size=32,
+            ref_attention_dropout=0.2,
+            token_num=32,
+            predictor_kernel_size=5,
+        )
     )
-    variance_adaptor: VarianceAdaptorConfig = VarianceAdaptorConfig(
-        n_hidden=384, kernel_size=5, p_dropout=0.5, n_bins=256
+    variance_adaptor: VarianceAdaptorConfig = field(
+        default_factory=lambda: VarianceAdaptorConfig(
+            n_hidden=384, kernel_size=5, p_dropout=0.5, n_bins=256
+        )
     )
 
 
@@ -159,38 +173,46 @@ class AcousticENModelConfig:
 class AcousticMultilingualModelConfig:
     speaker_embed_dim: int = 1024
     lang_embed_dim: int = 256
-    encoder: ConformerConfig = ConformerConfig(
-        n_layers=6,
-        n_heads=8,
-        n_hidden=512,
-        p_dropout=0.1,
-        kernel_size_conv_mod=7,
-        kernel_size_depthwise=7,
-        with_ff=True,
+    encoder: ConformerConfig = field(
+        default_factory=lambda: ConformerConfig(
+            n_layers=6,
+            n_heads=8,
+            n_hidden=512,
+            p_dropout=0.1,
+            kernel_size_conv_mod=7,
+            kernel_size_depthwise=7,
+            with_ff=True,
+        )
     )
-    decoder: ConformerConfig = ConformerConfig(
-        n_layers=6,
-        n_heads=8,
-        n_hidden=512,
-        p_dropout=0.1,
-        kernel_size_conv_mod=11,
-        kernel_size_depthwise=11,
-        with_ff=True,
+    decoder: ConformerConfig = field(
+        default_factory=lambda: ConformerConfig(
+            n_layers=6,
+            n_heads=8,
+            n_hidden=512,
+            p_dropout=0.1,
+            kernel_size_conv_mod=11,
+            kernel_size_depthwise=11,
+            with_ff=True,
+        )
     )
-    reference_encoder: ReferenceEncoderConfig = ReferenceEncoderConfig(
-        bottleneck_size_p=4,
-        bottleneck_size_u=256,
-        ref_enc_filters=[32, 32, 64, 64, 128, 128],
-        ref_enc_size=3,
-        ref_enc_strides=[1, 2, 1, 2, 1],
-        ref_enc_pad=[1, 1],
-        ref_enc_gru_size=32,
-        ref_attention_dropout=0.2,
-        token_num=32,
-        predictor_kernel_size=5,
+    reference_encoder: ReferenceEncoderConfig = field(
+        default_factory=lambda: ReferenceEncoderConfig(
+            bottleneck_size_p=4,
+            bottleneck_size_u=256,
+            ref_enc_filters=[32, 32, 64, 64, 128, 128],
+            ref_enc_size=3,
+            ref_enc_strides=[1, 2, 1, 2, 1],
+            ref_enc_pad=[1, 1],
+            ref_enc_gru_size=32,
+            ref_attention_dropout=0.2,
+            token_num=32,
+            predictor_kernel_size=5,
+        )
     )
-    variance_adaptor: VarianceAdaptorConfig = VarianceAdaptorConfig(
-        n_hidden=512, kernel_size=5, p_dropout=0.5, n_bins=256
+    variance_adaptor: VarianceAdaptorConfig = field(
+        default_factory=lambda: VarianceAdaptorConfig(
+            n_hidden=512, kernel_size=5, p_dropout=0.5, n_bins=256
+        )
     )
 
 
@@ -259,25 +281,31 @@ class VocoderMRDConfig:
 
 @dataclass
 class VocoderModelConfig:
-    gen: VocoderGeneratorConfig = VocoderGeneratorConfig(
-        noise_dim=64,
-        channel_size=32,
-        dilations=[1, 3, 9, 27],
-        strides=[8, 8, 4],
-        lReLU_slope=0.2,
-        kpnet_conv_size=3,
+    gen: VocoderGeneratorConfig = field(
+        default_factory=lambda: VocoderGeneratorConfig(
+            noise_dim=64,
+            channel_size=32,
+            dilations=[1, 3, 9, 27],
+            strides=[8, 8, 4],
+            lReLU_slope=0.2,
+            kpnet_conv_size=3,
+        )
     )
-    mpd: VocoderMPDConfig = VocoderMPDConfig(
-        periods=[2, 3, 5, 7, 11],
-        kernel_size=5,
-        stride=3,
-        use_spectral_norm=False,
-        lReLU_slope=0.2,
+    mpd: VocoderMPDConfig = field(
+        default_factory=lambda: VocoderMPDConfig(
+            periods=[2, 3, 5, 7, 11],
+            kernel_size=5,
+            stride=3,
+            use_spectral_norm=False,
+            lReLU_slope=0.2,
+        )
     )
-    mrd: VocoderMRDConfig = VocoderMRDConfig(
-        resolutions=[(1024, 120, 600), (2048, 240, 1200), (512, 50, 240)],
-        use_spectral_norm=False,
-        lReLU_slope=0.2,
+    mrd: VocoderMRDConfig = field(
+        default_factory=lambda: VocoderMRDConfig(
+            resolutions=[(1024, 120, 600), (2048, 240, 1200), (512, 50, 240)],
+            use_spectral_norm=False,
+            lReLU_slope=0.2,
+        )
     )
 
 
