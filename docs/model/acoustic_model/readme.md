37488c10886a369bbb96cdef0553f57837adf542
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-09-19 17:17:21 +0200

    Add embedding with tests, docs

--- docs/model/acoustic_model/readme.md
+++ docs/model/acoustic_model/readme.md
@@ -1,5 +1,9 @@
 ## References
 
+### [Embedding](embedding.md)
+
+This class represents a simple embedding layer but without any learning of the embeddings.
+
 ### [Variance Predictor](variance_predictor.md)
 
 This is a Duration and Pitch predictor neural network module in PyTorch.
---
+++ model/acoustic_model/embedding.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Embedding(nn.Module):
+    r"""
+    This class represents a simple embedding layer but without any learning of the embeddings.
+    The embeddings are initialized with random values and kept static throughout training (They are parameters, not model's state).
+
+    Args:
+        num_embeddings (int): Size of the dictionary of embeddings, typically size of the vocabulary.
+        embedding_dim (int): The size of each embedding vector.
+
+    Returns:
+        torch.Tensor: An output tensor resulting from the lookup operation.
+    """
+    def __init__(self, num_embeddings: int, embedding_dim: int):
+        super().__init__()
+        self.embeddings = nn.Parameter(torch.randn(num_embeddings, embedding_dim))
+
+    def forward(self, idx: torch.Tensor) -> torch.Tensor:
+        r"""
+        Forward propagation for the Embedding implementation.
+
+        Args:
+            idx (torch.Tensor): A tensor containing the indices of the embeddings to be accessed.
+            
+        Returns:
+            torch.Tensor: An output tensor resulting from the lookup operation.
+        """
+        x = F.embedding(idx, self.embeddings)
+        return x
