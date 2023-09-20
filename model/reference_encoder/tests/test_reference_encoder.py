fa7d0eebace988f4971cf5b5308237752b7233e9
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-09-20 17:54:38 +0200

    Drop mocks

--- model/reference_encoder/tests/test_reference_encoder.py
+++ model/reference_encoder/tests/test_reference_encoder.py
@@ -1,7 +1,8 @@
 import unittest
-from unittest.mock import Mock
 
 import torch
+
+from config import AcousticENModelConfig, PreprocessingConfig
 
 from model.reference_encoder import ReferenceEncoder
 
@@ -9,28 +10,9 @@ from model.reference_encoder import ReferenceEncoder
 class TestReferenceEncoder(unittest.TestCase):
     def setUp(self):
 
-        self.preprocess_config = Mock(stft=Mock(
-            filter_length=1024,
-            hop_length=256,
-            win_length=1024,
-            n_mel_channels=100,
-            mel_fmin=20,
-            mel_fmax=11025,
-        ))
-        self.model_config = Mock(
-            reference_encoder=Mock(
-                bottleneck_size_p=4,
-                bottleneck_size_u=256,
-                ref_enc_filters=[32, 32, 64, 64, 128, 128],
-                ref_enc_size=3,
-                ref_enc_strides=[1, 2, 1, 2, 1],
-                ref_enc_pad=[1, 1],
-                ref_enc_gru_size=32,
-                ref_attention_dropout=0.2,
-                token_num=32,
-                predictor_kernel_size=5,
-            )
-        )
+        self.preprocess_config = PreprocessingConfig("english_only")
+        self.model_config = AcousticENModelConfig()
+
         self.model = ReferenceEncoder(self.preprocess_config, self.model_config)
 
     def test_forward_shape(self):
--- model/reference_encoder/tests/test_utterance_level_prosody_encoder.py
+++ model/reference_encoder/tests/test_utterance_level_prosody_encoder.py
@@ -1,7 +1,8 @@
 import unittest
-from unittest.mock import Mock
 
 import torch
+
+from config import AcousticENModelConfig, PreprocessingConfig
 
 from model.reference_encoder import UtteranceLevelProsodyEncoder
 
@@ -11,37 +12,8 @@ class TestUtteranceLevelProsodyEncoder(unittest.TestCase):
     @classmethod
     def setUp(self):
 
-        self.preprocess_config = Mock(stft=Mock(
-            filter_length=1024,
-            hop_length=256,
-            win_length=1024,
-            n_mel_channels=100,
-            mel_fmin=20,
-            mel_fmax=11025,
-        ))
-        self.model_config = Mock(
-            reference_encoder=Mock(
-                bottleneck_size_p=4,
-                bottleneck_size_u=256,
-                ref_enc_filters=[32, 32, 64, 64, 128, 128],
-                ref_enc_size=3,
-                ref_enc_strides=[1, 2, 1, 2, 1],
-                ref_enc_pad=[1, 1],
-                ref_enc_gru_size=32,
-                ref_attention_dropout=0.2,
-                token_num=32,
-                predictor_kernel_size=5,
-            ),
-            encoder=Mock(
-                n_layers=4,
-                n_heads=6,
-                n_hidden=384,
-                p_dropout=0.1,
-                kernel_size_conv_mod=7,
-                kernel_size_depthwise=7,
-                with_ff=False,
-            )
-        )
+        self.preprocess_config = PreprocessingConfig("english_only")
+        self.model_config = AcousticENModelConfig()
         
         # Instantiate the model to be tested with the mock configurations
         self.model = UtteranceLevelProsodyEncoder(self.preprocess_config, self.model_config)
