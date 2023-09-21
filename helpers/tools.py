bc9ac86c8dc25a6bc6aee21d87e5cea8e353aa7b
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-09-21 15:26:44 +0200

    Add calc_same_padding

--- helpers/tools.py
+++ helpers/tools.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 
-from typing import List
+from typing import List, Tuple
 
 
 def pad(input_ele: List[torch.Tensor], max_len: int) -> torch.Tensor:
@@ -95,3 +95,29 @@ def stride_lens_downsampling(lens: torch.Tensor, stride: int = 2) -> torch.Tenso
     # each item is present at least once in the downsampled lengths.
     # Finally, the .int() is used to convert the resulting float32 tensor to an integer tensor.
     return torch.ceil(lens / stride).int()
+
+
+def calc_same_padding(kernel_size: int) -> Tuple[int, int]:
+    r"""
+    Calculates the necessary padding for 'same' padding in convolutional operations.
+
+    For 'same' padding, the output size is the same as the input size for `stride=1`. This function returns
+    two integers, representing the padding to be added on either side of the input to achieve 'same' padding.
+
+    Args:
+        kernel_size (int): Size of the convolving kernel.
+
+    Returns:
+        Tuple[int, int]: A tuple of two integers representing the number of padding elements to be applied on
+        left and right (or top and bottom for 2D) of the input tensor respectively.
+    """
+    # Check if kernel_size is an integer greater than zero
+    if not isinstance(kernel_size, int) or kernel_size <= 0:
+        raise ValueError("kernel_size must be an integer greater than zero")
+
+    # Determine base padding amount (equal to half the kernel size, truncated down)
+    pad = kernel_size // 2
+
+    # Return padding for each side of the kernel. If kernel size is odd, padding is (pad, pad).
+    # If kernel size is even, padding is (pad, pad - 1) because we can't pad equally on both sides.
+    return (pad, pad - (kernel_size + 1) % 2)
