7de516b602d65269bf945008f948ebd52076fa18
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-09-25 15:13:35 +0200

    Add test of conformer block

--- model/attention/tests/test_conformer_block.py
+++ model/attention/tests/test_conformer_block.py
@@ -1,46 +1,114 @@
 import unittest
+
 import torch
 
+from config import (
+    AcousticENModelConfig,
+    AcousticPretrainingConfig,
+    PreprocessingConfig,
+)
 
+from model.acoustic_model.helpers import positional_encoding
 from model.attention.conformer_block import ConformerBlock
+
+from helpers.initializer import (
+    init_acoustic_model,
+    init_conformer,
+    init_forward_trains_params,
+    init_mask_input_embeddings_encoding_attn_mask,
+)
+import helpers.tools as tools
 
 
 # @todo: it's one of the most important component test
 # But it's too complicated to cover it from the first glance.
 # You need to come back here when acoustic model is ready!
 class TestConformerBlock(unittest.TestCase):
+    def setUp(self):
+        self.acoustic_pretraining_config = AcousticPretrainingConfig()
+        self.model_config = AcousticENModelConfig()
+        self.preprocess_config = PreprocessingConfig("english_only")
+
+        # Based on speaker.json mock
+        n_speakers = 10
+
+        # Init conformer config
+        _, self.conformer_config = init_conformer(self.model_config)
+
+        # Add AcousticModel instance
+        self.acoustic_model, _ = init_acoustic_model(
+            self.preprocess_config, self.model_config, n_speakers
+        )
+
+        d_k = d_v = self.conformer_config.dim // self.conformer_config.n_heads
+
+        self.model = ConformerBlock(
+            d_model=self.conformer_config.dim,
+            n_head=self.conformer_config.n_heads,
+            d_k=d_k,
+            d_v=d_v,
+            kernel_size_conv_mod=self.conformer_config.kernel_size_conv_mod,
+            embedding_dim=self.conformer_config.embedding_dim,
+            dropout=self.conformer_config.p_dropout,
+            with_ff=self.conformer_config.with_ff,
+        )
+
+        # Generate mock data for the forward pass
+        self.forward_train_params = init_forward_trains_params(
+            self.model_config,
+            self.acoustic_pretraining_config,
+            self.preprocess_config,
+            n_speakers,
+        )
+
     def test_initialization(self):
         """
         Test for successful creation of ConformerBlock instance.
         """
-        model = ConformerBlock(
-            d_model=12,
-            n_head=6,
-            d_k=12,
-            d_v=12,
-            kernel_size_conv_mod=3,
-            embedding_dim=12,
-            dropout=0.1,
-            with_ff=False,
+        self.assertIsInstance(self.model, ConformerBlock)
+
+    def test_forward(self):
+        """
+        Test for successful forward pass.
+        """
+        (
+            src_mask,
+            x,
+            embeddings,
+            encoding,
+            attn_mask,
+        ) = init_mask_input_embeddings_encoding_attn_mask(
+            self.acoustic_model,
+            self.forward_train_params,
+            self.model_config,
         )
-        self.assertIsInstance(model, ConformerBlock)
+
+        # Run conformer block model
+        # x: Tensor containing the encoded sequences. Shape: [speaker_embed_dim, batch_size, speaker_embed_dim]
+        x = self.model(
+            x,
+            mask=src_mask,
+            slf_attn_mask=attn_mask,
+            embeddings=embeddings,
+            encoding=encoding,
+        )
+
+        # Assert the shape of x
+        self.assertEqual(
+            x.shape,
+            torch.Size(
+                [
+                    self.model_config.speaker_embed_dim,
+                    self.acoustic_pretraining_config.batch_size,
+                    self.model_config.speaker_embed_dim,
+                ]
+            ),
+        )
 
     def test_with_ff_flag(self):
         """
         Test for correct response based on `with_ff` flag during initialization.
         """
-        model = ConformerBlock(
-            d_model=12,
-            n_head=6,
-            d_k=12,
-            d_v=12,
-            kernel_size_conv_mod=3,
-            embedding_dim=12,
-            dropout=0.1,
-            with_ff=True,
-        )
-        self.assertTrue(hasattr(model, "ff"))
-
         model = ConformerBlock(
             d_model=20,
             n_head=5,
@@ -52,6 +120,18 @@ class TestConformerBlock(unittest.TestCase):
             with_ff=False,
         )
         self.assertFalse(hasattr(model, "ff"))
+
+        model = ConformerBlock(
+            d_model=12,
+            n_head=6,
+            d_k=12,
+            d_v=12,
+            kernel_size_conv_mod=3,
+            embedding_dim=12,
+            dropout=0.1,
+            with_ff=True,
+        )
+        self.assertTrue(hasattr(model, "ff"))
 
 
 if __name__ == "__main__":
