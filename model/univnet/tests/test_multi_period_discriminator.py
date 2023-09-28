c78ab3ca45107f0124b4dd7e5bcbec38c0a9b173
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-09-28 12:05:08 +0200

    Add assert the shape of the feature maps explicitly

--- model/univnet/tests/test_multi_period_discriminator.py
+++ model/univnet/tests/test_multi_period_discriminator.py
@@ -27,25 +27,84 @@ class TestMultiPeriodDiscriminator(unittest.TestCase):
 
         self.assertEqual(len(output), len(self.model_config.mpd.periods))
 
+        fmaps_dims = [
+            [
+                torch.Size([self.batch_size, 64, 17, self.model_config.mpd.periods[0]]),
+                torch.Size([self.batch_size, 128, 6, self.model_config.mpd.periods[0]]),
+                torch.Size([self.batch_size, 256, 2, self.model_config.mpd.periods[0]]),
+                torch.Size([self.batch_size, 512, 1, self.model_config.mpd.periods[0]]),
+                torch.Size(
+                    [self.batch_size, 1024, 1, self.model_config.mpd.periods[0]]
+                ),
+            ],
+            [
+                torch.Size([self.batch_size, 64, 12, self.model_config.mpd.periods[1]]),
+                torch.Size([self.batch_size, 128, 4, self.model_config.mpd.periods[1]]),
+                torch.Size([self.batch_size, 256, 2, self.model_config.mpd.periods[1]]),
+                torch.Size([self.batch_size, 512, 1, self.model_config.mpd.periods[1]]),
+                torch.Size(
+                    [self.batch_size, 1024, 1, self.model_config.mpd.periods[1]]
+                ),
+            ],
+            [
+                torch.Size([self.batch_size, 64, 7, self.model_config.mpd.periods[2]]),
+                torch.Size([self.batch_size, 128, 3, self.model_config.mpd.periods[2]]),
+                torch.Size([self.batch_size, 256, 1, self.model_config.mpd.periods[2]]),
+                torch.Size([self.batch_size, 512, 1, self.model_config.mpd.periods[2]]),
+                torch.Size(
+                    [self.batch_size, 1024, 1, self.model_config.mpd.periods[2]]
+                ),
+            ],
+            [
+                torch.Size([self.batch_size, 64, 5, self.model_config.mpd.periods[3]]),
+                torch.Size([self.batch_size, 128, 2, self.model_config.mpd.periods[3]]),
+                torch.Size([self.batch_size, 256, 1, self.model_config.mpd.periods[3]]),
+                torch.Size([self.batch_size, 512, 1, self.model_config.mpd.periods[3]]),
+                torch.Size(
+                    [self.batch_size, 1024, 1, self.model_config.mpd.periods[3]]
+                ),
+            ],
+            [
+                torch.Size([self.batch_size, 64, 4, self.model_config.mpd.periods[4]]),
+                torch.Size([self.batch_size, 128, 2, self.model_config.mpd.periods[4]]),
+                torch.Size([self.batch_size, 256, 1, self.model_config.mpd.periods[4]]),
+                torch.Size([self.batch_size, 512, 1, self.model_config.mpd.periods[4]]),
+                torch.Size(
+                    [self.batch_size, 1024, 1, self.model_config.mpd.periods[4]]
+                ),
+            ],
+        ]
+
         init_2nd_dims = [17, 12, 7, 5, 4]
 
         for mpd_k in range(len(self.model_config.mpd.periods)):
             fmap = output[mpd_k][0]
             x = output[mpd_k][1]
 
+            # Assert the device
+            self.assertEqual(x.device.type, self.device.type)
+
             self.assertEqual(len(x), self.batch_size)
 
+            # Assert the shape of the feature maps
             dim_2nd = init_2nd_dims[mpd_k]
             period = self.model_config.mpd.periods[mpd_k]
 
+            dims_expl = fmaps_dims[mpd_k]
+
             for i in range(len(self.model_config.mpd.periods)):
-                self.assertEqual(fmap[i].shape[0], self.batch_size)
-                self.assertEqual(fmap[i].shape[1], 2 ** (i + 6))
-                self.assertEqual(fmap[i].shape[2], dim_2nd)
+                # Assert the device
+                self.assertEqual(fmap[i].device.type, self.device.type)
 
+                # Assert the shape of the feature maps explicitly
+                self.assertEqual(fmap[i].shape, dims_expl[i])
+
+                # Assert the shape of the feature maps
+                self.assertEqual(
+                    fmap[i].shape,
+                    torch.Size([self.batch_size, 2 ** (i + 6), dim_2nd, period]),
+                )
                 dim_2nd = math.ceil(dim_2nd / self.model_config.mpd.stride)
-
-                self.assertEqual(fmap[i].shape[3], period)
 
         self.assertEqual(len(output), len(self.model_config.mpd.periods))
 
--- model/univnet/tests/test_multi_resolution_discriminator.py
+++ model/univnet/tests/test_multi_resolution_discriminator.py
@@ -20,10 +20,64 @@ class TestMultiResolutionDiscriminator(unittest.TestCase):
     def test_forward(self):
         # Test the forward pass of the MultiResolutionDiscriminator class
         output = self.model(self.x)
-
         self.assertEqual(len(output), 3)
 
-        # output shape is to unpredicatable to cover...
+        fmap_dims = [
+            [
+                torch.Size([32, 1, 513]),
+                torch.Size([32, 1, 257]),
+                torch.Size([32, 1, 129]),
+                torch.Size([32, 1, 65]),
+                torch.Size([32, 1, 65]),
+                torch.Size([1, 65]),
+            ],
+            [
+                torch.Size([32, 1, 1025]),
+                torch.Size([32, 1, 513]),
+                torch.Size([32, 1, 257]),
+                torch.Size([32, 1, 129]),
+                torch.Size([32, 1, 129]),
+                torch.Size([1, 129]),
+            ],
+            [
+                torch.Size([32, 1, 257]),
+                torch.Size([32, 1, 129]),
+                torch.Size([32, 1, 65]),
+                torch.Size([32, 1, 33]),
+                torch.Size([32, 1, 33]),
+                torch.Size([1, 33]),
+            ],
+        ]
+
+        init_powers_max_min = [(9, 6), (10, 7), (8, 5)]
+
+        for key in range(len(output)):
+            fmap = output[key][0]
+            x = output[key][1]
+
+            # Assert the device
+            self.assertEqual(x.device.type, self.device.type)
+
+            first_dim, second_dim = 32, 1
+
+            p_max, p_min = init_powers_max_min[key]
+
+            def dim_3rd(p: int):
+                return max(2**p + 1, 2**p_min + 1)
+
+            fmap_dim = fmap_dims[key]
+
+            # Assert the shape of the feature maps
+            for i, fmap in enumerate(fmap[:-1]):
+                # Assert the feature map shape explicitly
+                self.assertEqual(fmap.shape, fmap_dim[i])
+
+                self.assertEqual(
+                    fmap.shape, torch.Size([first_dim, second_dim, dim_3rd(p_max - i)])
+                )
+
+            print(fmap[-1].shape, torch.Size([second_dim, 2**p_min + 1]))
+            self.assertEqual(fmap[-1].shape, torch.Size([second_dim, 2**p_min + 1]))
 
 
 if __name__ == "__main__":
