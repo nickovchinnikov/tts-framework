e161797a39b6dfa04d473a7f93ae2c102feaffbb
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-10-04 18:07:12 +0200

    Add byte_encode, stereo_to_mono, resample, safe_load, preprocess_audio

--- training/preprocess/tests/test_tools.py
+++ training/preprocess/tests/test_tools.py
@@ -1,12 +1,18 @@
+import math
 import unittest
 
 import numpy as np
+import torch
 
 from training.preprocess.tools import (
+    byte_encode,
     compute_yin,
     cumulativeMeanNormalizedDifferenceFunction,
     differenceFunction,
     getPitch,
+    preprocess_audio,
+    resample,
+    stereo_to_mono,
 )
 
 
@@ -98,6 +104,100 @@ class TestTools(unittest.TestCase):
         expected_output = np.load("mocks/test_compute_yin.npy")
 
         np.testing.assert_allclose(actual_output, expected_output)
+
+    def test_byte_encode(self):
+        # Test with a simple word
+        word = "hello"
+        expected_output = [104, 101, 108, 108, 111]
+        self.assertTrue(byte_encode(word) == expected_output)
+
+        # Test with a word containing non-ASCII characters
+        word = "héllo"
+        expected_output = [104, 195, 169, 108, 108, 111]
+        self.assertTrue(byte_encode(word) == expected_output)
+
+        # Test with an empty string
+        word = ""
+        expected_output = []
+        self.assertTrue(byte_encode(word) == expected_output)
+
+    def test_stereo_to_mono(self):
+        # Test the stereo_to_mono function with a simple example
+        audio = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+        expected_output = torch.tensor([[2.5, 3.5, 4.5]])
+        actual_output = stereo_to_mono(audio)
+
+        self.assertTrue(torch.allclose(actual_output, expected_output))
+
+        # Test the stereo_to_mono function with a larger example
+        audio = torch.randn(2, 44100)
+        expected_output = torch.mean(audio, axis=0, keepdims=True)
+        actual_output = stereo_to_mono(audio)
+        self.assertTrue(torch.allclose(actual_output, expected_output))
+
+        # Test the stereo_to_mono function with a zero-dimensional tensor
+        audio = torch.tensor(1.0)
+        expected_output = torch.tensor([[1.0]])
+        actual_output = stereo_to_mono(audio)
+        self.assertTrue(torch.allclose(actual_output, expected_output))
+
+        # Test the stereo_to_mono function with a one-dimensional tensor
+        audio = torch.tensor([1.0, 2.0, 3.0])
+        expected_output = torch.tensor([2.0])
+        actual_output = stereo_to_mono(audio)
+        self.assertTrue(torch.allclose(actual_output, expected_output))
+
+        # Test the stereo_to_mono function with a three-dimensional tensor
+        audio = torch.randn(2, 3, 44100)
+        expected_output = torch.mean(audio, axis=0, keepdims=True)
+        actual_output = stereo_to_mono(audio)
+        self.assertTrue(torch.allclose(actual_output, expected_output))
+
+    def test_resample(self):
+        # Test the resample function with a simple example
+        wav = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+        orig_sr = 44100
+        target_sr = 22050
+
+        # Test the output shape
+        expected_shape = math.ceil(
+            len(wav) / 2,
+        )
+        actual_output = resample(wav, orig_sr, target_sr)
+        self.assertEqual(actual_output.shape, (expected_shape,))
+
+        # Test the output values
+        expected_output = np.array([0.62589299, 3.51319581, 0.0])
+        actual_output = resample(wav, orig_sr, target_sr)
+        np.testing.assert_allclose(actual_output, expected_output, rtol=1e-6, atol=1e-6)
+
+    def test_preprocess_audio(self):
+        # Test the preprocess_audio function with a simple example
+        audio = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+        sr_actual = 44100
+        sr = 22050
+
+        # Test the output shape
+        expected_shape = torch.Size([2])
+        actual_output, actual_sr = preprocess_audio(audio, sr_actual, sr)
+        self.assertEqual(actual_output.shape, expected_shape)
+
+        # Test the output values
+        expected_output = torch.tensor([2.434131, 0.000000])
+        np.testing.assert_allclose(actual_output, expected_output, rtol=1e-6, atol=1e-6)
+        self.assertEqual(actual_sr, sr)
+
+        # Test the preprocess_audio function with a three-dimensional tensor
+        audio = torch.randn(2, 3, 44100)
+        sr_actual = 44100
+        sr = 22050
+
+        # Test the output shape
+        expected_shape = torch.Size([3, len(audio[0][0]) // 2])
+        actual_output, actual_sr = preprocess_audio(audio, sr_actual, sr)
+
+        self.assertEqual(actual_output.shape, expected_shape)
+        self.assertEqual(actual_sr, sr)
 
 
 if __name__ == "__main__":
--- training/preprocess/tools.py
+++ training/preprocess/tools.py
@@ -1,8 +1,11 @@
-from typing import List, Tuple
+import sys
+from typing import List, Tuple, Union
 
+import librosa
 import numpy as np
 import torch
 import torch.nn.functional as F
+import torchaudio
 
 ######################################################################
 # Original implementation from https://github.com/NVIDIA/mellotron/blob/master/yin.py
@@ -179,3 +182,106 @@ def compute_yin(
             harmonic_rates[i] = min(cmdf)
 
     return np.array(pitches), harmonic_rates, argmins, times
+
+
+def byte_encode(word):
+    r"""
+    Encode a word as a list of bytes.
+
+    Args:
+        word (str): The word to encode.
+
+    Returns:
+        list: A list of bytes representing the encoded word.
+    """
+    text = word.strip()
+    return list(text.encode("utf-8"))
+
+
+def stereo_to_mono(audio: torch.FloatTensor) -> torch.FloatTensor:
+    r"""
+    Converts a stereo audio tensor to mono by taking the mean across channels.
+
+    Args:
+        audio (torch.FloatTensor): Input audio tensor of shape (channels, samples).
+
+    Returns:
+        torch.FloatTensor: Mono audio tensor of shape (1, samples).
+    """
+    return torch.mean(audio, axis=0, keepdims=True)
+
+
+def resample(wav: np.ndarray, orig_sr: int, target_sr: int) -> np.ndarray:
+    r"""
+    Resamples an audio waveform from the original sampling rate to the target sampling rate.
+
+    Args:
+        wav (np.ndarray): The audio waveform to be resampled.
+        orig_sr (int): The original sampling rate of the audio waveform.
+        target_sr (int): The target sampling rate to resample the audio waveform to.
+
+    Returns:
+        np.ndarray: The resampled audio waveform.
+    """
+    wav = librosa.resample(wav, orig_sr=orig_sr, target_sr=target_sr)
+    return wav
+
+
+def safe_load(path: str, sr: Union[int, None]) -> Tuple[np.ndarray, int]:
+    r"""
+    Load an audio file from disk and return its content as a numpy array.
+
+    Args:
+        path (str): The path to the audio file.
+        sr (int or None): The target sampling rate. If None, the original sampling rate is used.
+
+    Returns:
+        Tuple[np.ndarray, int]: A tuple containing the audio content as a numpy array and the actual sampling rate.
+    """
+    try:
+        audio, sr_actual = torchaudio.load(path)
+        if audio.shape[0] > 0:
+            audio = stereo_to_mono(audio)
+        audio = audio.squeeze(0)
+        if sr_actual != sr and sr is not None:
+            audio = resample(audio.numpy(), orig_sr=sr_actual, target_sr=sr)
+            sr_actual = sr
+        else:
+            audio = audio.numpy()
+    except Exception as e:
+        raise type(e)(
+            f"The following error happened loading the file {path} ... \n" + str(e)
+        ).with_traceback(sys.exc_info()[2])
+
+    return audio, sr_actual
+
+
+def preprocess_audio(
+    audio: torch.Tensor, sr_actual: int, sr: Union[int, None]
+) -> Tuple[torch.Tensor, int]:
+    r"""
+    Preprocesses audio by converting stereo to mono, resampling if necessary, and returning the audio tensor and sample rate.
+
+    Args:
+        audio (torch.Tensor): The audio tensor to preprocess.
+        sr_actual (int): The actual sample rate of the audio.
+        sr (Union[int, None]): The target sample rate to resample the audio to, if necessary.
+
+    Returns:
+        Tuple[torch.Tensor, int]: The preprocessed audio tensor and sample rate.
+    """
+    try:
+        if audio.shape[0] > 0:
+            audio = stereo_to_mono(audio)
+        audio = audio.squeeze(0)
+        if sr_actual != sr and sr is not None:
+            audio = resample(audio.numpy(), orig_sr=sr_actual, target_sr=sr)
+            # Convert back to torch tensor
+            audio = torch.from_numpy(audio)
+            sr_actual = sr
+    except Exception as e:
+        raise type(e)(
+            f"The following error happened while processing the audio ... \n {str(e)}"
+        ).with_traceback(sys.exc_info()[2])
+
+    return audio, sr_actual
