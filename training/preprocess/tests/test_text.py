705904c3997baea491056b106d42fb32f904ec41
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-10-11 12:25:13 +0200

    Add normalize_chars

--- training/preprocess/tests/test_text.py
+++ training/preprocess/tests/test_text.py
@@ -2,9 +2,7 @@ import unittest
 
 import numpy as np
 
-from training.preprocess.text import (
-    byte_encode,
-)
+from training.preprocess.text import byte_encode, normalize_chars
 
 
 # Create a class to test the ComputePitch class
@@ -27,3 +25,43 @@ class TestTextPreprocess(unittest.TestCase):
         word = ""
         expected_output = []
         self.assertTrue(byte_encode(word) == expected_output)
+
+    def test_normalize_chars(self):
+        # Test case 1: Test basic character normalization
+        input_text = "It’s a beautiful day…"
+        expected_output = "It's a beautiful day."
+        self.assertEqual(normalize_chars(input_text), expected_output)
+
+        # Test case 2: Test character normalization with multiple dots
+        input_text = "Hello..... world!!!!"
+        expected_output = "Hello. world!"
+        self.assertEqual(normalize_chars(input_text), expected_output)
+
+        # Test case 3: Test character normalization with multiple exclamation marks
+        input_text = "Wow!!!!! This is amazing?????"
+        expected_output = "Wow! This is amazing?"
+        self.assertEqual(normalize_chars(input_text), expected_output)
+
+        # Test case 4: Test character normalization with multiple question marks
+        input_text = "What????? I don't understand!????"
+        expected_output = "What? I don't understand!?"
+        self.assertEqual(normalize_chars(input_text), expected_output)
+
+        # Test case 5: Test character normalization with multiple quotes
+        input_text = 'He said, “I don’t know…”'
+        expected_output = 'He said, "I don\'t know."'
+        self.assertEqual(normalize_chars(input_text), expected_output)
+
+        # Test case 6: Test character normalization with multiple dashes
+        input_text = "This is a long--sentence"
+        expected_output = "This is a long-sentence"
+        self.assertEqual(normalize_chars(input_text), expected_output)
+
+        # Test case 7: Test character normalization with mixed characters
+        input_text = "It’s a beautiful day… What????? I don't understand!!!!!"
+        expected_output = "It's a beautiful day. What? I don't understand!"
+        self.assertEqual(normalize_chars(input_text), expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- training/preprocess/text.py
+++ training/preprocess/text.py
@@ -1,3 +1,8 @@
+import re
+
+from unidecode import unidecode
+
+
 def byte_encode(word: str) -> list:
     r"""
     Encode a word as a list of bytes.
@@ -7,6 +12,49 @@ def byte_encode(word: str) -> list:
 
     Returns:
         list: A list of bytes representing the encoded word.
+
     """
     text = word.strip()
     return list(text.encode("utf-8"))
+
+
+def normalize_chars(text: str) -> str:
+    r"""
+    Normalize the characters in the input text.
+
+    Args:
+        text (str): The input text to normalize.
+
+    Returns:
+        str: The normalized text.
+
+    Examples:
+        >>> normalize_chars("It’s a beautiful day…")
+        "It's a beautiful day..."
+
+    """
+    # Define the character mapping
+    char_mapping = {
+        ord("’"): ord("'"),
+        ord("”"): ord('"'),
+        ord("…"): ord("."),
+        ord("„"): ord('"'),
+        ord("“"): ord('"'),
+        ord("–"): ord("-"),
+        ord("«"): ord('"'),
+        ord("»"): ord('"'),
+    }
+
+    # Normalize the characters using translate() method
+    normalized_string = text.translate(char_mapping)
+    
+    # Add unicode normalization as additional garanty
+    normalized_string = unidecode(normalized_string)
+    
+    # Remove redundant multiple characters
+    normalized_string = re.sub(r"(\.|\!|\?|\-)\1+", r"\1",normalized_string)
+    # TODO: Maybe there is some effect on duplication?
+    # re.sub(r"(\.|\!|\?)\1{2,}", r"\1\1\1", normalized_string)
+
+
+    return normalized_string
