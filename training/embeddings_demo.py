f393478f31951384b9de367e6ac384cbc4aa680a
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-10-12 08:35:56 +0200

    Create vocab for tokenization

--- training/embeddings_demo.py
+++ training/embeddings_demo.py
@@ -8,7 +8,7 @@ model = BertModel.from_pretrained('bert-base-uncased')
 
 phonemizer = Phonemizer.from_checkpoint("../checkpoints/en_us_cmudict_ipa_forward.pt")
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', vocab_file='../checkpoints/vocab2.txt')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', vocab_file='../checkpoints/merged_vocab.txt')
 
 configuration = BertConfig()
 configuration
@@ -39,8 +39,32 @@ text = "".join(phonemes_example)
 text
 
 # %%
+# Create bert dict
 phone2ixd = json.load(open("../config/phone2idx.json"))
 for key in phone2ixd.keys():
     print(key)
+
+
+
+# %%
+# Create a new vocabulary
+# Load the existing BERT vocabulary file
+bert_vocab_file = "../checkpoints/vocab.txt"
+with open(bert_vocab_file, encoding="utf-8") as f:
+    base_vocab = {line.strip(): i for i, line in enumerate(f)}
+
+# Load the additional vocabulary from vocab2.txt
+vocab2_file = "../checkpoints/vocab2.txt"
+with open(vocab2_file, encoding="utf-8") as f:
+    vocab2 = {line.strip(): i for i, line in enumerate(f)}
+
+# Merge the two vocabularies
+merged_vocab = {**base_vocab, **vocab2}
+
+# Save the merged vocabulary to a new file
+merged_vocab_file = "../checkpoints/merged_vocab.txt"
+with open(merged_vocab_file, "w", encoding="utf-8") as f:
+    for key, value in merged_vocab.items():
+        f.write(f"{key}\n")
 
 # %%
