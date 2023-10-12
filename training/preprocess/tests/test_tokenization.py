374ba4a8aeb9ea6fa7067cfd51441ebe31e35ab0
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-10-12 09:04:04 +0200

    Add tokenization

---
+++ training/preprocess/tests/test_tokenization.py
@@ -0,0 +1,51 @@
+import unittest
+
+from training.preprocess.tokenization import Tokenizer
+
+
+class TestTokenizer(unittest.TestCase):
+    def setUp(self):
+        self.tokenizer = Tokenizer()
+
+    def test_tokenization(self):
+        text = "This is a test sentence."
+        expected_tokens = [101, 2025, 2005, 1039, 3233, 6253, 1014, 102]
+        tokens = self.tokenizer(text)
+        self.assertEqual(tokens, expected_tokens)
+
+    def test_decode(self):
+        tokens = [101, 2025, 2005, 1039, 3233, 6253, 1014, 102]
+        expected_text = "this is a test sentence."
+        text = self.tokenizer.decode(tokens)
+        self.assertEqual(text, expected_text)
+
+    def test_tokenization_with_vocab_file(self):
+        text = "This is a test sentence."
+        vocab_file = "config/vocab_phonemes.txt"
+        tokenizer = Tokenizer(vocab_file=vocab_file)
+        expected_tokens = [64, 2, 2, 2, 2, 2, 2, 63]
+        tokens = tokenizer(text)
+        self.assertEqual(tokens, expected_tokens)
+
+    def test_decode_with_special_tokens(self):
+        tokens = [101, 2025, 2005, 1039, 3233, 6253, 1014, 102]
+        expected_text = "[CLS] this is a test sentence. [SEP]"
+        text = self.tokenizer.decode(tokens)
+        self.assertEqual(text, expected_text)
+
+    def test_encode_decode_phonemes(self):
+        text = ['[SILENCE]', 'ð', 'ʌ', '[SILENCE]', 'd', 'eɪ', '[SILENCE]', 'æ', 'f', 't', 'ɜ˞', '[COMMA]', 'd', 'aɪ', 'æ', 'n', 'ʌ', '[SILENCE]', 'æ', 'n', 'd', '[SILENCE]', 'm', 'ɛ', 'ɹ', 'i', '[SILENCE]', 'k', 'w', 'ɪ', 't', 'ɪ', 'd', '[SILENCE]', 'ɪ', 't', '[SILENCE]', 'f', 'ɜ˞', '[SILENCE]', 'd', 'ɪ', 's', 't', 'ʌ', 'n', 't', '[SILENCE]', 'b', 'i', '[FULL STOP]']
+        
+        expected_tokens = [101, 104, 1100, 1136, 104, 1042, 30531, 104, 1099, 1044, 1058, 30535, 30524, 1042, 30528, 1099, 1052, 1136, 104, 1099, 1052, 1042, 104, 1051, 1117, 1127, 1047, 104, 1049, 1061, 1121, 1058, 1121, 1042, 104, 1121, 1058, 104, 1044, 30535, 104, 1042, 1121, 1057, 1058, 1136, 1052, 1058, 104, 1040, 1047, 105, 102]
+
+        tokens = self.tokenizer(text)
+        self.assertEqual(tokens, expected_tokens)
+
+        expected_decoded_text = '[CLS] [SILENCE] ð ʌ [SILENCE] d eɪ [SILENCE] æ f t ɜ˞ [COMMA] d aɪ æ n ʌ [SILENCE] æ n d [SILENCE] m ɛ ɹ i [SILENCE] k w ɪ t ɪ d [SILENCE] ɪ t [SILENCE] f ɜ˞ [SILENCE] d ɪ s t ʌ n t [SILENCE] b i [FULL STOP] [SEP]'
+        decoded_phonemes = self.tokenizer.decode(tokens)
+
+        self.assertEqual(decoded_phonemes, expected_decoded_text)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ training/preprocess/tokenization.py
@@ -0,0 +1,63 @@
+from transformers import BertTokenizer
+
+
+class Tokenizer:
+    r"""
+    A wrapper class for the BERT tokenizer from the Hugging Face Transformers library.
+    Use this with `vocab_file` and it makes sure that the correct vocabulary is used.
+
+    Args:
+        checkpoint (str): The name or path of the pre-trained BERT checkpoint to use.
+        vocab_file (str): The path to the custom vocabulary file to use (optional).
+
+    Attributes:
+        tokenizer (BertTokenizer): The BERT tokenizer object.
+
+    """
+    
+    def __init__(self, checkpoint: str = "bert-base-uncased", vocab_file:str = "config/vocab.txt") -> None:
+        r"""
+        Initializes the Tokenizer object with the specified checkpoint and vocabulary file.
+
+        Args:
+            checkpoint (str): The name or path of the pre-trained BERT checkpoint to use.
+            vocab_file (str): The path to the custom vocabulary file to use (optional).
+
+        Returns:
+            None.
+
+        """
+
+        self.tokenizer = BertTokenizer.from_pretrained(checkpoint, vocab_file=vocab_file)
+
+    def __call__(self, text: str, add_special_tokens: bool = True) -> list[int]:
+        r"""
+        Tokenizes the input text using the BERT tokenizer.
+
+        Args:
+            text (str): The input text to tokenize.
+            add_special_tokens (bool): Whether to add special tokens to the tokenized text (optional).
+
+        Returns:
+            tokens (List[int]): A list of token IDs representing the tokenized text.
+
+        """
+
+        tokens = self.tokenizer.encode(text, add_special_tokens=add_special_tokens)
+        return tokens
+    
+    def decode(self, tokens: list[int], add_special_tokens: bool = False) -> list[str]:
+        r"""
+        Decodes the input token IDs into a list of strings.
+
+        Args:
+            tokens (List[int]): A list of token IDs to decode.
+            add_special_tokens (bool): Whether to add special tokens to the tokenized text (optional).
+
+        Returns:
+            text (List[str]): A list of strings representing the decoded tokens.
+
+        """
+
+        text_list = self.tokenizer.decode(tokens, skip_special_tokens=add_special_tokens)
+        return text_list
