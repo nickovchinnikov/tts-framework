91027005e904c23eb9bd7c7d3065acb2e03ed996
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-10-12 13:25:52 +0200

    Download the aligned dataset

---
+++ training/datasets/libritts-aligned_demo.py
@@ -0,0 +1,20 @@
+# %%
+# ruff: noqa: E402
+import os
+
+LIBRITTS_PATH = "../../datasets_cache"
+
+os.environ["LIBRITTS_PATH"] = LIBRITTS_PATH
+
+from datasets import load_dataset
+
+dataset = load_dataset('cdminix/libritts-r-aligned', data_dir="../../datasets_cache", split="train")
+
+# %%
+dataset[0]
+
+# %%
+
+# %%
+
+# %%
