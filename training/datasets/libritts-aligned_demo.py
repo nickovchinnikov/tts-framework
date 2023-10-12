1780c3fdf38be88181d3790e44518a42fec100df
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-10-12 18:38:25 +0200

    Found a solution for Wav2VecAligner Adaptation in progress

--- training/datasets/libritts-aligned_demo.py
+++ training/datasets/libritts-aligned_demo.py
@@ -6,6 +6,8 @@ LIBRITTS_PATH = "../../datasets_cache"
 
 os.environ["LIBRITTS_PATH"] = LIBRITTS_PATH
 
+print(LIBRITTS_PATH)
+
 from datasets import load_dataset
 
 dataset = load_dataset('cdminix/libritts-r-aligned', data_dir="../../datasets_cache", split="train")
