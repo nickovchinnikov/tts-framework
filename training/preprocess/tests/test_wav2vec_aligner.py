e305350444ca7d9bfbc22d42b4014f095964cb55
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-10-13 13:25:19 +0200

    Add wav2vec_aligner

--- training/preprocess/tests/test_wav2vec_aligner.py
+++ training/preprocess/tests/test_wav2vec_aligner.py
@@ -23,20 +23,67 @@ class TestWav2VecAligner(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             self.model.load_audio("./nonexistent/path.wav")
 
-    def test_text_to_transcript(self):
-        transcript = self.model.text_to_transcript(self.text)
-        expected_transcript = "VILLEFORT|ROSE,|HALF|ASHAMED|OF|BEING|SURPRISED|IN|SUCH|A|PAROXYSM|OF|GRIEF."
-        self.assertEqual(transcript, expected_transcript)
-
     def test_align_single_sample(self):
         audio_input, _ = self.model.load_audio(self.wav_path)
-        emissions, tokens = self.model.align_single_sample(
+        emissions, tokens, transcript = self.model.align_single_sample(
             audio_input, self.text
         )
         expected_emissions = torch.load("./mocks/wav2vec_aligner/emissions.pt")
         
         torch.testing.assert_close(emissions, expected_emissions)
-        self.assertEqual(tokens, [26, 11, 16, 16, 6, 21, 9, 14, 7, 14, 9, 13, 6, 12, 8, 16, 21, 8, 13, 12, 8, 18, 6, 15, 9, 21, 25, 6, 11, 10, 22, 13, 17, 14, 24, 14, 11, 13, 6, 15, 11, 10, 13, 17, 20, 12, 8, 24, 8, 14, 9, 29, 23, 13, 18, 9, 21, 22, 14, 11, 6, 21])
+
+        self.assertEqual(tokens, [26, 11, 16, 16, 6, 21, 9, 14, 7, 5, 14, 9, 13, 6, 5, 12, 8, 16, 21, 5, 8, 13, 12, 8, 18, 6, 15, 5, 9, 21, 5, 25, 6, 11, 10, 22, 5, 13, 17, 14, 24, 14, 11, 13, 6, 15, 5, 11, 10, 5, 13, 17, 20, 12, 5, 8, 5, 24, 8, 14, 9, 29, 23, 13, 18, 5, 9, 21, 5, 22, 14, 11, 6, 21])
+        
+        self.assertEqual(transcript, "VILLEFORT|ROSE,|HALF|ASHAMED|OF|BEING|SURPRISED|IN|SUCH|A|PAROXYSM|OF|GRIEF.")
+
+    def test_get_trellis(self):
+        audio_input, _ = self.model.load_audio(self.wav_path)
+        emissions, tokens, _ = self.model.align_single_sample(audio_input, self.text)
+        trellis = self.model.get_trellis(emissions, tokens)
+
+        expected_trellis = torch.load("./mocks/wav2vec_aligner/trellis.pt")
+
+        # Add assertions here based on the expected behavior of get_trellis
+        self.assertIsInstance(trellis, torch.Tensor)
+        torch.testing.assert_close(trellis, expected_trellis)
+
+    def test_backtrack(self):
+        audio_input, _ = self.model.load_audio(self.wav_path)
+        emissions, tokens, _ = self.model.align_single_sample(audio_input, self.text)
+        trellis = self.model.get_trellis(emissions, tokens)
+        path = self.model.backtrack(trellis, emissions, tokens)
+
+        expected_path = torch.load("./mocks/wav2vec_aligner/backtrack_path.pt")
+
+        # Add assertions here based on the expected behavior of backtrack
+        self.assertIsInstance(path, list)
+        self.assertEqual(path, expected_path)
+
+    def test_merge_repeats(self):
+        audio_input, _ = self.model.load_audio(self.wav_path)
+        emissions, tokens, transcript = self.model.align_single_sample(audio_input, self.text)
+        trellis = self.model.get_trellis(emissions, tokens)
+        path = self.model.backtrack(trellis, emissions, tokens)
+        merged_path = self.model.merge_repeats(path, transcript)
+
+        expected_merged_path = torch.load("./mocks/wav2vec_aligner/merged_path.pt")
+
+        # Add assertions here based on the expected behavior of merge_repeats
+        self.assertIsInstance(merged_path, list)
+        self.assertEqual(merged_path, expected_merged_path)
+
+    def test_merge_words(self):
+        audio_input, _ = self.model.load_audio(self.wav_path)
+        emissions, tokens, transcript = self.model.align_single_sample(audio_input, self.text)
+        trellis = self.model.get_trellis(emissions, tokens)
+        path = self.model.backtrack(trellis, emissions, tokens)
+        merged_path = self.model.merge_repeats(path, transcript)
+        words = self.model.merge_words(merged_path)
+
+        # Add assertions here based on the expected behavior of merge_words
+        self.assertIsInstance(words, list)
+
+
 
 if __name__ == "__main__":
     unittest.main()
--- training/preprocess/wav2vec_aligner.py
+++ training/preprocess/wav2vec_aligner.py
@@ -1,18 +1,14 @@
 from dataclasses import dataclass
 import os
-from typing import Any, Tuple
+from typing import Tuple
 
 from lightning.pytorch import LightningModule
-import numpy as np
-import soundfile as sf
 import torch
 import torchaudio
 from transformers import (
     AutoConfig,
     AutoModelForCTC,
     AutoProcessor,
-    Wav2Vec2ForCTC,
-    Wav2Vec2Processor,
 )
 
 
@@ -22,12 +18,43 @@ class Item:
     wav_path: str
     out_path: str
 
+    
+@dataclass
+class Point:
+    token_index: int
+    time_index: int
+    score: float
+
+
+@dataclass
+class Segment:
+    label: str
+    start: int
+    end: int
+    score: float
+
+    def __repr__(self):
+        return f"{self.label}\t{self.score:4.2f}\t{self.start*20:5d}\t{self.end*20:5d}"
+
+    @property
+    def length(self):
+        return self.end - self.start
+
+
 class Wav2VecAligner(LightningModule):
     def __init__(
             self,
             input_wavs_sr: int = 48_000,
             model_name: str = "facebook/wav2vec2-base-960h",
         ):
+        r"""
+        Initialize a new instance of the Wav2VecAligner class.
+
+        Args:
+            input_wavs_sr (int): The sample rate of the input wave files. Defaults to 48_000.
+            model_name (str): The name of the pre-trained model to use. Defaults to "facebook/wav2vec2-base-960h".
+        """
+
         super().__init__()
 
         # Load the config
@@ -50,24 +77,41 @@ class Wav2VecAligner(LightningModule):
         self.blank_id = blank_id
 
 
-    def load_audio(self, wav_path: str) -> Tuple[torch.Tensor, int, FileNotFoundError]:
+    def load_audio(self, wav_path: str) -> Tuple[torch.Tensor, int]:
+        r"""
+        Load an audio file from the specified path.
+
+        Args:
+            wav_path (str): The path to the audio file.
+
+        Returns:
+            Tuple[torch.Tensor, int]: A tuple containing the loaded audio data and the sample rate, or a FileNotFoundError if the file does not exist.
+        """
+
         if not os.path.isfile(wav_path):
             raise FileNotFoundError(wav_path, "Not found in wavs directory")
 
         speech_array, sampling_rate = torchaudio.load(wav_path)
         return speech_array, sampling_rate
-    
-
-    def text_to_transcript(self, text: str) -> str:
-        result = "|".join(text.split(" "))
-        return result
 
 
     def align_single_sample(
         self,
         speech_array: torch.Tensor,
-        transcript: str
-    ):
+        text: str
+    ) -> Tuple[torch.Tensor, list, str]:
+        r"""
+        Align a single sample of audio data with the corresponding text.
+
+        Args:
+            speech_array (torch.Tensor): The audio data.
+            text (str): The corresponding text.
+
+        Returns:
+            Tuple[torch.Tensor, list, str]: A tuple containing the emissions, the tokens, and the transcript.
+        """
+
+        transcript = "|".join(text.split(" "))
         speech_array = self.resampler(speech_array).squeeze()
 
         inputs = self.processor(
@@ -81,9 +125,11 @@ class Wav2VecAligner(LightningModule):
         with torch.no_grad():
             logits = self.model(inputs.input_values).logits
 
-        # get the emission probability at frame level
+        # Get the emission probability at frame level
+        # Compute the probability in log-domain to avoid numerical instability
+        # For this purpose, we normalize the emission with `torch.log_softmax()`
         emissions = torch.log_softmax(logits, dim=-1)
-        emissions[0].cpu().detach()
+        emissions = emissions[0]
 
         # get labels from vocab
         labels = ([""] + list(self.processor.tokenizer.get_vocab().keys()))[
@@ -96,5 +142,186 @@ class Wav2VecAligner(LightningModule):
             if c in dictionary:
                 tokens.append(dictionary[c])
 
-        return emissions, tokens
+        return emissions, tokens, transcript
+    
 
+    def get_trellis(
+        self,
+        emission: torch.Tensor,
+        tokens: list,
+        blank_id: int = 0
+    ) -> torch.Tensor:
+        r"""
+        Build a trellis matrix of shape (num_frames + 1, num_tokens + 1)
+        that represents the probabilities of each source token being at a certain time step.
+
+        Since we are looking for the most likely transitions, we take the more likely path for the value of $k_{(t+1,j+1)}$​, that is:
+
+        $k_{t+1, j+1} = \max(k_{t, j} p_{t+1, c_{j+1}}, k_{t, j+1} p_{t+1, \text{repeat}})$
+
+        Args:
+            emission (torch.Tensor): The emission tensor.
+            tokens (list): The list of tokens.
+            blank_id (int): The ID of the blank token. Defaults to 0.
+
+        Returns:
+            torch.Tensor: The trellis matrix.
+        """
+        num_frames = emission.size(0)
+        num_tokens = len(tokens)
+
+        # Trellis has extra diemsions for both time axis and tokens.
+        # The extra dim for tokens represents <SoS> (start-of-sentence)
+        # The extra dim for time axis is for simplification of the code.
+        trellis = torch.full((num_frames + 1, num_tokens + 1), -float("inf"))
+        trellis[:, 0] = 0
+        for t in range(num_frames):
+            trellis[t + 1, 1:] = torch.maximum(
+                # Score for staying at the same token
+                trellis[t, 1:] + emission[t, blank_id],
+                # Score for changing to the next token
+                trellis[t, :-1] + emission[t, tokens],
+            )
+        return trellis
+    
+
+    def backtrack(
+        self,
+        trellis: torch.Tensor,
+        emission: torch.Tensor,
+        tokens: list,
+        blank_id: int=0
+    ) -> list[Point]:
+        r"""
+        Walk backwards from the last (sentence_token, time_step) pair to build the optimal sequence alignment path.
+
+        Args:
+            trellis (torch.Tensor): The trellis matrix.
+            emission (torch.Tensor): The emission tensor.
+            tokens (list): The list of tokens.
+            blank_id (int): The ID of the blank token. Defaults to 0.
+
+        Returns:
+            list[Point]: The optimal sequence alignment path.
+        """
+        # Note:
+        # j and t are indices for trellis, which has extra dimensions
+        # for time and tokens at the beginning.
+        # When referring to time frame index `T` in trellis,
+        # the corresponding index in emission is `T-1`.
+        # Similarly, when referring to token index `J` in trellis,
+        # the corresponding index in transcript is `J-1`.
+        j = trellis.size(1) - 1
+        t_start = int(torch.argmax(trellis[:, j]).item())
+
+        path: list[Point] = []
+        for t in range(t_start, 0, -1):
+            # 1. Figure out if the current position was stay or change
+            # Note (again):
+            # `emission[J-1]` is the emission at time frame `J` of trellis dimension.
+            # Score for token staying the same from time frame J-1 to T.
+            stayed = trellis[t - 1, j] + emission[t - 1, blank_id]
+            # Score for token changing from C-1 at T-1 to J at T.
+            changed = trellis[t - 1, j - 1] + emission[t - 1, tokens[j - 1]]
+
+            # 2. Store the path with frame-wise probability.
+            prob = emission[t - 1, tokens[j - 1] if changed > stayed else 0].exp().item()
+            # Return token index and time index in non-trellis coordinate.
+            path.append(Point(j - 1, t - 1, prob))
+
+            # 3. Update the token
+            if changed > stayed:
+                j -= 1
+                if j == 0:
+                    break
+        else:
+            raise ValueError("Failed to align")
+        return path[::-1]
+    
+    
+    def merge_repeats(self, path: list[Point], transcript: str) -> list[Segment]:
+        r"""
+        Merge repeated tokens into a single segment.
+
+        Args:
+            path (list[Point]): The sequence alignment path.
+            transcript (str): The transcript.
+
+        Returns:
+            list[Segment]: The list of segments.
+
+        Note: this shouldn't affect repeated characters from the
+        original sentences (e.g. `ll` in `hello`)
+        """
+        i1, i2 = 0, 0
+        segments = []
+        while i1 < len(path):
+            while i2 < len(path) and path[i1].token_index == path[i2].token_index:
+                i2 += 1
+            score = sum(path[k].score for k in range(i1, i2)) / (i2 - i1)
+            segments.append(
+                Segment(
+                    transcript[path[i1].token_index],
+                    path[i1].time_index,
+                    path[i2 - 1].time_index + 1,
+                    score,
+                )
+            )
+            i1 = i2
+        return segments
+    
+
+    # Merge words
+    def merge_words(self, segments: list[Segment], separator="|") -> list[Segment]:
+        r"""
+        Merge words in the given path.
+
+        Args:
+            segments (list[Segment]): The list of segments.
+            separator (str): The separator character. Defaults to "|".
+
+        Returns:
+            list[Segment]: The list of merged words.
+        """
+        words = []
+        i1, i2 = 0, 0
+        while i1 < len(segments):
+            if i2 >= len(segments) or segments[i2].label == separator:
+                if i1 != i2:
+                    segs = segments[i1:i2]
+                    word = "".join([seg.label for seg in segs])
+                    score = sum(seg.score * seg.length for seg in segs) / sum(seg.length for seg in segs)
+                    words.append(Segment(word, segments[i1].start, segments[i2 - 1].end, score))
+                i1 = i2 + 1
+                i2 = i1
+            else:
+                i2 += 1
+        return words
+
+
+    def forward(self, wav_path: str, text: str) -> list[Segment]:
+        r"""
+        Perform the forward pass of the model, which involves loading the audio data, aligning the audio with the text,
+        building the trellis, backtracking to find the optimal path, merging repeated tokens, and finally merging words.
+
+        Args:
+            wav_path (str): The path to the audio file.
+            text (str): The corresponding text.
+
+        Returns:
+            list[Segment]: The list of segments representing the alignment of the audio data with the text.
+        """
+
+        audio_input, _ = self.load_audio(wav_path)
+        
+        emissions, tokens, transcript = self.align_single_sample(audio_input, text)
+
+        trellis = self.get_trellis(emissions, tokens)
+
+        path = self.backtrack(trellis, emissions, tokens)
+
+        merged_path = self.merge_repeats(path, transcript)
+
+        result = self.merge_words(merged_path)
+
+        return result
