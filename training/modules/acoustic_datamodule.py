dda075022f2d336c1fb8273db5c4d0c8c3e307fd
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-11-02 11:25:04 +0100

    Update docs AcousticDataModule useless ??

---
+++ training/modules/acoustic_datamodule.py
@@ -0,0 +1,22 @@
+from lightning.pytorch.core import LightningDataModule
+from torch.utils.data import DataLoader
+
+from training.datasets import LibriTTSDatasetAcoustic
+
+
+class AcousticDataModule(LightningDataModule):
+    def __init__(self, data_dir: str = "path/to/dir", batch_size: int = 3):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+
+    def setup(self, stage: str) -> None:
+        self.dataset = LibriTTSDatasetAcoustic()
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            collate_fn=self.dataset.collate_fn,
+        )
