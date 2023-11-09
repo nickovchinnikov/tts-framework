86900997548e6255e255eec1cbb6b5d7f50f45e9
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2023-11-09 10:42:16 +0100

    Add preprocessed dataset

--- training/datasets/__init__.py
+++ training/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .libritts_dataset_acoustic import LibriTTSDatasetAcoustic
 from .libritts_dataset_vocoder import LibriTTSDatasetVocoder
 from .libritts_mm_dataset_acoustic import LibriTTSMMDatasetAcoustic
+from .preprocessed_dataset import PreprocessedDataset
