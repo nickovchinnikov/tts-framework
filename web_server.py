418b679f2def5fbc32d7b779c40e86a880e36d47
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2024-01-04 13:47:57 +0100

    Add CPU endpoint Attempt to fix the streaming

--- web_server.py
+++ web_server.py
@@ -2,6 +2,8 @@ import os
 
 from pydantic import BaseModel, Field
 
+from pydub import AudioSegment
+
 import asyncio
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
@@ -22,18 +24,29 @@ os.environ["NUMBA_DISABLE_INTEL_SVML"] = "1"
 
 
 # Load the pretrained weights from the checkpoint
-checkpoint = "checkpoints/epoch=5155-step=524125.ckpt"
-module = AcousticModule.load_from_checkpoint(checkpoint)
+checkpoint = "checkpoints/epoch=5482-step=601951.ckpt"
 
+device_cpu = torch.device("cpu")
+module_cpu = AcousticModule.load_from_checkpoint(checkpoint).to(device_cpu)
 # Set the module to eval mode
-module.eval()
+module_cpu.eval()
+
+
+device_gpu = torch.device("cuda")
+module_gpu = AcousticModule.load_from_checkpoint(checkpoint).to(device_gpu)
+# Set the module to eval mode
+module_gpu.eval()
 
 
 # Load the speaker information
 existed_speakers = speakers_info()
 
+BIT_RATE = 128  # kbps
 SAMPLING_RATE = 22050
 AUDIO_FORMAT = "mp3"
+
+# FrameSize = 144 * BitRate / (SampleRate + Padding)
+FRAME_SIZE = int((144 * BIT_RATE) / (SAMPLING_RATE * 0.001))
 
 
 class TransformerParams(BaseModel):
@@ -41,7 +54,7 @@ class TransformerParams(BaseModel):
     speaker: str = Field(default="122") # Default speaker "carnright"
 
 
-async def async_gen(text: str, speaker: str):
+async def async_gen(text: str, speaker: torch.Tensor, module: AcousticModule):
     """Audio streaming async generator function."""
     try:
         sentences = sentences_split(text)
@@ -51,9 +64,7 @@ async def async_gen(text: str, speaker: str):
             with torch.no_grad():
                 wav_prediction = module(
                     text,
-                    torch.tensor([
-                        int(speaker)
-                    ]),
+                    speaker,
                 )
 
                 buffer_ = returnAudioBuffer(
@@ -61,7 +72,14 @@ async def async_gen(text: str, speaker: str):
                     SAMPLING_RATE,
                     AUDIO_FORMAT
                 )
-                yield buffer_.read()
+
+                audio = AudioSegment.from_file(buffer_, format=AUDIO_FORMAT)
+
+                # Stream the audio in frame-sized chunks
+                frame_size = FRAME_SIZE  # This is the frame size for MP3 at 22.05kHz and 128 kbps. Adjust as necessary.
+                for i in range(0, len(audio), frame_size):
+                    yield audio[i:i+frame_size].raw_data
+
                 # need this sleep in order to be able to catch the disconnect
                 await asyncio.sleep(0)
 
@@ -86,9 +104,32 @@ def generate(params: TransformerParams):
     try:
         if params.speaker not in existed_speakers:
             raise HTTPException(status_code=400, detail="Speaker not found")
+        
+        speaker = torch.tensor([
+            int(params.speaker)
+        ], device=device_cpu)
 
         return StreamingResponse(
-            async_gen(params.text, params.speaker),
+            async_gen(params.text, speaker, module_cpu),
+            media_type=f"audio/{AUDIO_FORMAT}",
+        )
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/generate_cuda/")
+def generate_cuda(params: TransformerParams):
+    try:
+        if params.speaker not in existed_speakers:
+            raise HTTPException(status_code=400, detail="Speaker not found")
+
+        speaker = torch.tensor([
+            int(params.speaker)
+        ], device=device_gpu)
+
+        return StreamingResponse(
+            async_gen(params.text, speaker, module_gpu),
             media_type=f"audio/{AUDIO_FORMAT}",
         )
 
@@ -100,6 +141,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "web_server:app",
         host="0.0.0.0",
-        port=8080,
+        port=8000,
         log_level="info",
     )
