e0386fad601c6e9ff2e7fe8aaf0a680307a6c0b8
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2024-01-08 18:48:17 +0100

    Add scripts to setup the env

--- training/gcp/settings.sh
+++ training/gcp/settings.sh
@@ -1,3 +1,9 @@
+# Stop instance:
+# sudo shutdown -h now
+
+# Start instance:
+# gcloud workbench instances start prod-train  --location=europe-west4-a
+
 # CUDA drivers
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb 
 # sudo dpkg -i cuda-keyring_1.0-1_all.deb
@@ -15,37 +21,71 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install ffmpeg libasound2-dev -y
 
+mkdir tts-training-bucket
+
+# Mount the bucket
+gcsfuse tts-training-bucket "$HOME/tts-training-bucket"
+
+# Clone the repo
+git clone git@github.com:nickovchinnikov/tts-framework.git
+
 # Connect the bucket to the VM instance, cd tts_framework
-cp ../tts-training-bucket/vocoder.ckpt ./checkpoints/
-cp ../tts-training-bucket/en_us_cmudict_ipa_forward.pt ./checkpoints/
-cp ../tts-training-bucket/epoch\=5537-step\=615041.ckpt ./checkpoints/
+cp ./tts-training-bucket/vocoder.ckpt ./tts-framework/checkpoints/
+cp ./tts-training-bucket/en_us_cmudict_ipa_forward.pt ./tts-framework/checkpoints/
+cp ./tts-training-bucket/epoch\=5537-step\=615041.ckpt ./tts-framework/checkpoints/
 
 # Copy the datasets_cache folder metadata
-mkdir -p ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/BOOKS.TXT ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/CHAPTERS.TXT ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/eval_sentences10.tsv ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/LICENSE.txt ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/NOTE.txt ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/reader_book.tsv ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/README_librispeech.txt ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/README_libritts.txt ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/speakers.tsv ./datasets_cache/LIBRITTS/LibriTTS/
-cp ../tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/SPEAKERS.txt ./datasets_cache/LIBRITTS/LibriTTS/
+mkdir -p ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
 
-# Download the dataset
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/BOOKS.txt ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/CHAPTERS.txt ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/eval_sentences10.tsv ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/LICENSE.txt ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/NOTE.txt ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/reader_book.tsv ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/README_librispeech.txt ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/README_libritts.txt ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/speakers.tsv ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+cp ./tts-training-bucket/datasets_cache/LIBRITTS/LibriTTS/SPEAKERS.txt ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/
+
+#############################################################################################################
+######################################### LibriTTS-R dataset ################################################
+#############################################################################################################
+
+# Download the dataset train_clean_100
+curl -O https://us.openslr.org/resources/141/train_clean_100.tar.gz
+mv train_clean_100.tar.gz ./tts-framework/datasets_cache/LIBRITTS/
+
+tar -xzvf ./tts-framework/datasets_cache/LIBRITTS/train_clean_100.tar.gz -C ./tts-framework/datasets_cache/LIBRITTS/
+mv ./tts-framework/datasets_cache/LIBRITTS/LibriTTS_R/train-clean-100 ./tts-framework/datasets_cache/LIBRITTS/LibriTTS
+rm -r ./tts-framework/datasets_cache/LIBRITTS/LibriTTS_R
+
+# Download the dataset train_clean_360
 curl -O https://us.openslr.org/resources/141/train_clean_360.tar.gz
-mv train_clean_360.tar.gz ./datasets_cache/LIBRITTS/
+mv train_clean_360.tar.gz ./tts-framework/datasets_cache/LIBRITTS/
 
-tar -xzvf ./datasets_cache/LIBRITTS/train_clean_360.tar.gz -C ./datasets_cache/LIBRITTS/
-mv ./datasets_cache/LIBRITTS/LibriTTS_R/train-clean-360 ./datasets_cache/LIBRITTS/LibriTTS
-rm -r ./datasets_cache/LIBRITTS/LibriTTS_R
+tar -xzvf ./tts-framework/datasets_cache/LIBRITTS/train_clean_360.tar.gz -C ./tts-framework/datasets_cache/LIBRITTS/
+mv ./tts-framework/datasets_cache/LIBRITTS/LibriTTS_R/train-clean-360 ./tts-framework/datasets_cache/LIBRITTS/LibriTTS
+rm -r ./tts-framework/datasets_cache/LIBRITTS/LibriTTS_R
 
-conda env create -f environment.yml
+#############################################################################################################
+#########################################  LibriTTS dataset  ################################################
+#############################################################################################################
+
+curl -O https://us.openslr.org/resources/60/train-clean-100.tar.gz
+mv train-clean-100.tar.gz ./tts-framework/datasets_cache/LIBRITTS/
+
+tar -xzvf ./tts-framework/datasets_cache/LIBRITTS/train-clean-100.tar.gz -C ./tts-framework/datasets_cache/LIBRITTS/
+mv ./tts-framework/datasets_cache/LIBRITTS/LibriTTS/train-clean-100 ./tts-framework/datasets_cache/LIBRITTS/LibriTTS
+rm -r ./tts-framework/datasets_cache/LIBRITTS/LibriTTS_R
+
+#############################################################################################################
+
+conda env create -f ./tts-framework/environment.yml
 conda activate tts_framework
 
 pip install --upgrade --force-reinstall torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
-cp training/scripts/train_dist.py ./
+cp ./tts-framework/training/scripts/train_dist.py ./tts-framework
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/conda/lib
