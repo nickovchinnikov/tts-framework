Remove SWA from the DelightfulTTS Use new dataset train_dataloader Fix text normalization Move symbols to configs

models/tts/delightful_tts/delightful_tts_refined.py
@@ -5,7 +5,7 @@ from lightning.pytorch.core import LightningModule
 import soundfile as sf
 import torch
 from torch import Tensor
-from torch.optim import AdamW, swa_utils
+from torch.optim import AdamW
 from torch.optim.lr_scheduler import ExponentialLR
 from torch.utils.data import DataLoader
 from torchaudio.transforms import Resample
@@ -20,10 +20,9 @@ from models.config import (
     get_lang_map,
     lang2id,
 )
-from models.config.speakers import dataset_speaker_ids
-from models.helpers.dataloaders import train_dataloader
 from models.helpers.tools import get_mask_from_lengths
 from models.vocoder.univnet import UnivNet
+from training.datasets.hifi_libri_dataset import train_dataloader
 from training.loss import FastSpeech2LossGen
 from training.preprocess.normalize_text import NormalizeText
 
@@ -57,18 +56,14 @@ class DelightfulTTS(LightningModule):
         lang (str): Language of the dataset.
         n_speakers (int): Number of speakers in the dataset.generation during training.
         batch_size (int): The batch size.
-        swa_avg (bool): Whether to use SWA or not. Defaults to False. NOTE: When fine-tuning, SWA should be used!
     """
 
     def __init__(
         self,
         fine_tuning: bool = False,
         lang: str = "en",
-        # NOTE: lr finder found 1.5848931924611133e-07
-        # learning_rate: float = 1.5848931924611133e-05,
         n_speakers: int = 5392,
         batch_size: int = 4,
-        swa_avg: bool = True,
     ):
         super().__init__()
 
@@ -99,10 +94,6 @@ class DelightfulTTS(LightningModule):
             # NOTE: this parameter may be hyperparameter that you can define based on the demands
             n_speakers=n_speakers,
         )
-
-        self.swa_avg = swa_avg
-        # Initialize SWA
-        self.swa_averaged_model = swa_utils.AveragedModel(self.acoustic_model)
 
         # Initialize the vocoder, freeze for the first stage of the training
         self.vocoder_module = UnivNet()
@@ -360,33 +351,18 @@ class DelightfulTTS(LightningModule):
             "lr_scheduler": scheduler_acoustic,
         }
 
-    def on_train_epoch_end(self):
-        r"""Updates the averaged model after each optimizer step with SWA."""
-        if self.swa_avg:
-            self.swa_averaged_model.update_parameters(self.acoustic_model)
-
-    def on_train_end(self):
-        # Update SWA model after training
-        # if self.swa_avg:
-        #     swa_utils.update_bn(self.train_dataloader(), self.swa_averaged_model)
-        pass
-
     def train_dataloader(
         self,
-        root: str = "datasets_cache/LIBRITTS",
+        root: str = "datasets_cache",
         cache: bool = True,
-        cache_dir: str = "datasets_cache",
-        mem_cache: bool = False,
-        url: str = "train-960",
+        cache_dir: str = "/dev/shm",
     ) -> DataLoader:
         r"""Returns the training dataloader, that is using the LibriTTS dataset.
 
         Args:
             root (str): The root directory of the dataset.
             cache (bool): Whether to cache the preprocessed data.
-            cache_dir (str): The directory for the cache.
-            mem_cache (bool): Whether to use memory cache.
-            url (str): The URL of the dataset.
+            cache_dir (str): The directory for the cache. Defaults to "/dev/shm".
 
         Returns:
             Tupple[DataLoader, DataLoader]: The training and validation dataloaders.
@@ -397,8 +373,5 @@ class DelightfulTTS(LightningModule):
             root=root,
             cache=cache,
             cache_dir=cache_dir,
-            mem_cache=mem_cache,
-            url=url,
             lang=self.lang,
-            selected_speaker_ids=dataset_speaker_ids,
         )

training/datasets/hifi_libri_dataset.py
@@ -11,7 +11,7 @@ import numpy as np
 import soundfile as sf
 import torch
 from torch import Tensor
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset
 from voicefixer import VoiceFixer
 
 from models.config import PreprocessingConfig, get_lang_map, lang2id
@@ -496,3 +496,65 @@ class HifiLibriDataset(Dataset):
         std = torch.std(pitches_t).item()
 
         return min_value, max_value, mean, std
+
+
+def train_dataloader(
+    batch_size: int = 6,
+    num_workers: int = 5,
+    shuffle: bool = False,
+    lang: str = "en",
+    root: str = "datasets_cache",
+    hifitts_path: str = "hifitts",
+    hifi_cutset_file_name: str = "hifi.json.gz",
+    libritts_path: str = "librittsr",
+    libritts_cutset_file_name: str = "libri.json.gz",
+    libritts_subsets: List[str] | str = "all",
+    cache: bool = False,
+    cache_dir: str = "/dev/shm",
+) -> DataLoader:
+    r"""Returns the training dataloader, that is using the HifiLibriDataset dataset.
+
+    Args:
+        batch_size (int): The batch size.
+        num_workers (int): The number of workers.
+        shuffle (bool): Whether to shuffle the dataset.
+        lang (str): The language of the dataset.
+        root (str): The root directory of the dataset.
+        hifitts_path (str): The path to the HiFiTTS dataset.
+        hifi_cutset_file_name (str): The file name of the HiFiTTS cutset.
+        libritts_path (str): The path to the LibriTTS dataset.
+        libritts_cutset_file_name (str): The file name of the LibriTTS cutset.
+        libritts_subsets (List[str] | str): The subsets of the LibriTTS dataset to use.
+        cache (bool): Whether to cache the dataset.
+        cache_dir (str): The directory to cache the dataset in.
+
+    Returns:
+        DataLoader: The training dataloader.
+    """
+    dataset = HifiLibriDataset(
+        root=root,
+        hifitts_path=hifitts_path,
+        hifi_cutset_file_name=hifi_cutset_file_name,
+        libritts_path=libritts_path,
+        libritts_cutset_file_name=libritts_cutset_file_name,
+        libritts_subsets=libritts_subsets,
+        cache=cache,
+        cache_dir=cache_dir,
+        lang=lang,
+    )
+
+    train_loader = DataLoader(
+        dataset,
+        # 4x80Gb max 10 sec audio
+        # batch_size=20, # self.train_config.batch_size,
+        # 4*80Gb max ~20.4 sec audio
+        batch_size=batch_size,
+        # TODO: find the optimal num_workers
+        num_workers=num_workers,
+        persistent_workers=True,
+        pin_memory=True,
+        shuffle=shuffle,
+        collate_fn=dataset.collate_fn,
+    )
+
+    return train_loader