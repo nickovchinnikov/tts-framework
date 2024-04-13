abb7b1d06253f25ce00584376c0d6e01dbca2714
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2024-04-13 22:08:37 +0200

    Shrink the batch size

--- models/vocoder/hifigan/hifigan.py
+++ models/vocoder/hifigan/hifigan.py
@@ -32,7 +32,7 @@ class HifiGan(LightningModule):
         fine_tuning: bool = False,
         lang: str = "en",
         acc_grad_steps: int = 5,
-        batch_size: int = 10,
+        batch_size: int = 9,
         sampling_rate: int = 44100,
     ):
         r"""Initializes the `VocoderModule`.
