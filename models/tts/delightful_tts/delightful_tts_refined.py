bdd82600d9b540f83be84dada8aa8d38f47c6a4a
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2024-04-21 19:19:34 +0200

    Batch size changes

--- models/tts/delightful_tts/delightful_tts_refined.py
+++ models/tts/delightful_tts/delightful_tts_refined.py
@@ -43,7 +43,7 @@ class DelightfulTTS(LightningModule):
         fine_tuning: bool = False,
         lang: str = "en",
         n_speakers: int = 5392,
-        batch_size: int = 10,
+        batch_size: int = 15,
         sampling_rate: int = 22050,
     ):
         super().__init__()
